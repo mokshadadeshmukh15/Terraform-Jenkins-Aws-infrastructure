cf8a3850f383c91fc3d9f47a1f90cdb900be73ed
mokshadadeshmukh15 <167424235+mokshadadeshmukh15@users.noreply.github.com>
2025-07-25 13:08:02 +0530

    Update main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -67,10 +67,12 @@ resource "aws_route_table_association" "private" {
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
+
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
   vpc_id      = aws_vpc.main.id
-  description = "Allow HTTP from ALB"
+  description = "Allow traffic from VPC"
 
   ingress {
     from_port   = 80
@@ -87,16 +89,17 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
-resource "aws_security_group" "alb_sg" {
-  name        = "alb-sg"
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
   vpc_id      = aws_vpc.main.id
-  description = "Allow traffic from VPC"
+  description = "Allow HTTP from ALB"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+   security_groups = [aws_security_group.alb_sg.id]
   }
 
   egress {
@@ -119,10 +122,5 @@ resource "aws_security_group" "db_sg" {
     security_groups = [aws_security_group.web_sg.id]
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
 }
